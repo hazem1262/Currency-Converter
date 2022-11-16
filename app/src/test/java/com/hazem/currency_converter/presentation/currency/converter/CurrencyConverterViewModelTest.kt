d0dfb5cd045620ea52f62174505cf6956f0e7abe
Hazem Ashraf <hazem.ashraf@omniful.com>
2022-11-16 12:42:22 +0200

    add CurrencyConverterViewModel unit tests

---
+++ app/src/test/java/com/hazem/currency_converter/presentation/currency/converter/CurrencyConverterViewModelTest.kt
@@ -0,0 +1,5 @@
+package com.hazem.currency_converter.presentation.currency.converter
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CurrencyConverterViewModelTest
---
+++ app/src/test/java/com/hazem/currency_converter/utils/CurrencyModelFactory.kt
@@ -0,0 +1,4 @@
+package com.hazem.currency_converter.utils
+
+class CurrencyModelFactory {
+}
